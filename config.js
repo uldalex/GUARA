ee1bf0470dd8af1dc926c6b6e8b39885c60335aa
uldalex <uldalex@gmail.com>
2021-03-29 13:13:46 +0300

    Initial commit

---
+++ config.js
@@ -0,0 +1,43 @@
+/* global module */
+
+let config = {
+  'notGetBlocks': [
+    'blocks-demo.html',
+  ],
+  'ignoredBlocks': [
+    'no-js',
+  ],
+  'alwaysAddBlocks': [
+     'sprite-svg',
+     'sprite-png',
+     'object-fit-polyfill',
+  ],
+  'addStyleBefore': [
+    'src/scss/variables.scss',
+    'src/scss/mixins.scss',
+    'node_modules/@splidejs/splide/src/sass/core/splide-core.scss'
+  
+    
+    // 'somePackage/dist/somePackage.css', // для 'node_modules/somePackage/dist/somePackage.css',
+  ],
+  'addStyleAfter': [
+    // 'src/scss/print.scss',
+  ],
+  'addJsBefore': [
+    // 'somePackage/dist/somePackage.js', // для 'node_modules/somePackage/dist/somePackage.js',
+  ],
+  'addJsAfter': [
+    './script.js',
+  ],
+  'addAssets': {
+    "./src/img/*.{png,svg,jpg,jpeg}": "img/",
+     'node_modules/somePackage/images/*.{png,svg,jpg,jpeg}': 'img/',
+  },
+  'dir': {
+    'src': 'src/',
+    'build': 'build/',
+    'blocks': 'src/blocks/'
+  }
+};
+
+module.exports = config;
